f1257118109fb5564cb7bcda79fca05dec9e4e9f
raenjamio <renjamio@gmail.com>
2019-04-21 13:04:59 -0300

    Update CreateReservation.java

--- src/main/java/com/raenjamio/valtech/testdrive/api/usecase/reservation/CreateReservation.java
+++ src/main/java/com/raenjamio/valtech/testdrive/api/usecase/reservation/CreateReservation.java
@@ -71,7 +71,7 @@ public class CreateReservation {
 	 * @return
 	 */
 	
-	public ReservationDTO create(ReservationDTO reservationDTO) {
+	public synchronized ReservationDTO create(ReservationDTO reservationDTO) {
 		log.debug("create reservation");
 
 		CarDTO car = carService.findById(reservationDTO.getCarId());
